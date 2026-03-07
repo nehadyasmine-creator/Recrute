7416427638d92f3deec5991146307d11cfaf3f18
Yasmine NEHAD <nehadyasmine@gmail.com>
2026-03-07 12:00:07 +0100

    Forme Backend avec Spring

---
+++ recrute-backend/src/main/java/com/recrute/RecruteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.recrute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruteBackendApplication.class, args);
+	}
+
+}
---
+++ recrute-backend/src/test/java/com/recrute/RecruteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recrute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
