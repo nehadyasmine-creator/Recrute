3336d25aee7892744ab13a89545e4913d90d2acf
martin.ladan <martin.ladan@epfedu.fr>
2026-03-19 12:00:11 +0100

    ajout front avec composants

---
+++ projet-recrute/src/app/infos-offre/infos-offre.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-infos-offre',
+  imports: [],
+  templateUrl: './infos-offre.html',
+  styleUrl: './infos-offre.scss',
+})
+export class InfosOffre {
+
+}
---
+++ projet-recrute/src/app/postuler/postuler.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-postuler',
+  imports: [],
+  templateUrl: './postuler.html',
+  styleUrl: './postuler.scss',
+})
+export class Postuler {
+
+}
