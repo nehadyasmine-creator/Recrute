0c733ee45853b7d407126e1320361a9dec697d01
martin.ladan <martin.ladan@epfedu.fr>
2026-03-24 12:10:07 +0100

    structure + navbar + page de connexion/accueil déconnecté

--- projet-recrute/src/app/app.routes.ts
+++ projet-recrute/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Faq } from './faq/faq';
+import { Contact } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'faq', component: Faq },
+    { path: 'contactez-nous', component: Contact},
+    { path: '**', redirectTo: '' }
+];
