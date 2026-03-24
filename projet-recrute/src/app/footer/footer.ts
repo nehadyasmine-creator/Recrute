75a3e392f62a9fdb24602b6081606797a9f07698
GaetanP <gaetan.puiseux@epfedu.fr>
2026-03-24 11:30:47 +0100

    barre footer 1

--- projet-recrute/src/app/footer/footer.ts
+++ projet-recrute/src/app/footer/footer.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
+  standalone: true,
   imports: [],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss',
+  styleUrls: ['./footer.scss']
 })
-export class Footer {
-
-}
+export class FooterComponent {}
