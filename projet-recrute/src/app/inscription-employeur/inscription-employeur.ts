17fd1c0176e7ba71ef2cd1c1b7acb8c745c0fbf7
martin.ladan <martin.ladan@epfedu.fr>
2026-03-25 09:48:43 +0100

    ajout inscription candidat

---
+++ projet-recrute/src/app/inscription-employeur/inscription-employeur.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inscription-employeur',
+  imports: [],
+  templateUrl: './inscription-employeur.html',
+  styleUrl: './inscription-employeur.scss',
+})
+export class InscriptionEmployeur {
+
+}
