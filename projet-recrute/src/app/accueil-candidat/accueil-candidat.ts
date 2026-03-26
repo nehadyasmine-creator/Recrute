33afb83171e1f560c615d0f1ed5b3426cd1b4719
martin.ladan <martin.ladan@epfedu.fr>
2026-03-26 10:31:33 +0100

    accueil et création composants dashboard candidat et employeur

---
+++ projet-recrute/src/app/accueil-candidat/accueil-candidat.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accueil-candidat',
+  imports: [],
+  templateUrl: './accueil-candidat.html',
+  styleUrl: './accueil-candidat.scss',
+})
+export class AccueilCandidat {
+
+}
