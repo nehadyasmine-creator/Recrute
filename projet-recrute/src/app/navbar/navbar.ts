fa8f1ab047a0bf43729221d0a7878c122e1e59d7
martin.ladan <martin.ladan@epfedu.fr>
2026-03-27 09:13:51 +0100

    home, navbar, ajout pages

--- projet-recrute/src/app/navbar/navbar.ts
+++ projet-recrute/src/app/navbar/navbar.ts
@@ -1,11 +1,30 @@
+
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, NavigationEnd, RouterLink } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
+
 export class Navbar {
+  currentUrl: string = '';
+
+  constructor(private router: Router) {
+    // On écoute les changements de page
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      this.currentUrl = event.url;
+    });
+  }
+//faudra surement trouver une autre maniere de faire
+  isDeconnecte(): boolean {
+    return this.currentUrl === '/';
+  }
 
 }
