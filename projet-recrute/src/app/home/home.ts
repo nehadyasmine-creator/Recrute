94cc60d464b90de56a329cf684698b68d0e9f15d
martin.ladan <martin.ladan@epfedu.fr>
2026-04-01 11:19:33 +0200

    connexion fonctionnelle

--- projet-recrute/src/app/home/home.ts
+++ projet-recrute/src/app/home/home.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink , Router} from '@angular/router';
+import { AuthService } from '../service/auth.service';
+import { FormsModule } from '@angular/forms';
+
+
+interface Utilisateur{
+  id_utilisateur: number;
+  nom: string;
+  prenom: string;
+  email:string;
+  telephone:string;
+  motdepasse:string;
+}
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+  loginData = {
+    email:'',
+    password:''
+  }
+  errorMessage: string = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogin() {
+    this.authService.login(this.loginData.email, this.loginData.password).subscribe({
+      next: (response) => {
+        console.log('Connexion réussie !', response);
+        this.router.navigate(['/liste-offres']); 
+      },
+      error: (err) => {
+        this.errorMessage = "Identifiants incorrects. Veuillez réessayer.";
+        console.error(err);
+      }
+    });
+  }
 }
