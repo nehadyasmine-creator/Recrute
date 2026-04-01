c7fed7acf742cd08cd7d7d744dbe563ec3eab812
martin.ladan <martin.ladan@epfedu.fr>
2026-04-01 12:08:37 +0200

    déconnexion fonctionnelle, gestion navbar en fonction de l'etat de connexion

--- projet-recrute/src/app/navbar/navbar.ts
+++ projet-recrute/src/app/navbar/navbar.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, NavigationEnd, RouterLink } from '@angular/router';
 import { filter } from 'rxjs/operators';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -11,14 +12,19 @@ import { filter } from 'rxjs/operators';
 })
 
 export class Navbar {
+
+  isLoggedIn: boolean = false;
   currentUrl: string = '';
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private authService: AuthService) {
     // On écoute les changements de page
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
     ).subscribe((event: any) => {
       this.currentUrl = event.url;
+    });
+    this.authService.isLoggedIn$.subscribe(status => {
+      this.isLoggedIn = status;
     });
   }
 //faudra surement trouver une autre maniere de faire
--- projet-recrute/src/app/service/auth.service.ts
+++ projet-recrute/src/app/service/auth.service.ts
@@ -3,11 +3,18 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-
+  private loggedIn = new BehaviorSubject<boolean>(this.hasToken());
   private apiUrl = environment.apiUrl;
+
+  isLoggedIn$ = this.loggedIn.asObservable();
+
+  private hasToken(): boolean {
+    return !!localStorage.getItem('token');
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -19,12 +26,14 @@ export class AuthService {
     return this.http.post(`${this.apiUrl}/auth/login`, { email, motDePasse }).pipe(
       tap((response: any) => {
         localStorage.setItem('token', response.token);
+        this.loggedIn.next(true);
       })
     );
   }
 
   logout() {
     localStorage.removeItem('token');
+    this.loggedIn.next(false);
   }
 
   getToken(): string | null {
