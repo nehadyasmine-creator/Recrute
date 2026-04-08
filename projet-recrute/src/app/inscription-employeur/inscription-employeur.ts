c13a5fe66096b594bb422263d2681f14f9949a12
GaetanPsx <gaetanpsx@gmail.com>
2026-04-08 17:01:27 +0200

    employeur compte

--- projet-recrute/src/app/inscription-employeur/inscription-employeur.ts
+++ projet-recrute/src/app/inscription-employeur/inscription-employeur.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-inscription-employeur',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './inscription-employeur.html',
   styleUrl: './inscription-employeur.scss',
 })
 export class InscriptionEmployeur {
 
-}
+} 
